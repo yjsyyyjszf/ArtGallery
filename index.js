f68d6d9a657f6b5f81656a7fcfcdbe3b5a9700a5
dnp99 <pateldeep1001@gmail.com>
2020-02-09 19:16:03 -0500

    added branching on signup page

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 const www = process.env.WWW || './';
+
+
+//Mongo connection 
+const MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var multer = require('multer');
+var Schema = mongoose.Schema;
+const assert= require('assert');
+const uri = "mongodb+srv://deep:dnp@4283@cluster0-kdmbn.mongodb.net";
+const dbName = 'mayorwilson';
+
 app.use(express.static(www));
 
 console.log(`serving ${www}`);
@@ -16,5 +27,41 @@ app.get('/', (req, res) => {
 });
 
 app.use(/*route*/ '/', /*router*/ clientRouter);
+
+
+//mongo code
+//schema 
+var ItemSchema = new Schema(
+  { img: 
+      { data: Buffer, contentType: String }
+  }
+);
+var Item = mongoose.model('Clothes',ItemSchema);
+
+//multer middleware to  the path of the image we are uploading.
+app.use(multer({ dest: './uploads/',
+  rename: function (fieldname, filename) {
+    return filename;
+  },
+ }).any());
+
+
+ app.post('/api/photos',function(req,res){
+  var newItem = new Item();
+  newItem.img.data = fs.readFileSync(req.files.userPhoto.path)
+  newItem.img.contentType = 'image/jpg';
+  newItem.save();
+ });
+
+
+MongoClient.connect(uri, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to MongoDB");
+  const db = client.db(dbName);
+  //insertDocuments(db, function() {
+    // client.close();
+  // });
+  client.close();
+});
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
