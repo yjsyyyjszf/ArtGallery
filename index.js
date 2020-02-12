972dda1a0a8b7dc1f17bcfc8dcce1c344ec6eb2b
DESKTOP-4HGF5OF\ohyung <ohyung12871993@gmail.com>
2020-02-12 16:02:27 -0500

    sign up on blockchain as well

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const express = require('express');
+const express = require("express");
 var bodyParser = require("body-parser");
-const clientRouter = require('./server/routes/route');
-var User1 = require('../ArtGallry/public/schema/user');
+const clientRouter = require("./server/routes/route");
+// var User1 = require('../ArtGallry/public/schema/user');
 const app = express();
-const path = require('path');
-
+const path = require("path");
+const axios = require("axios");
 const port = process.env.PORT || 3000;
 
-const www = process.env.WWW || './';
+const www = process.env.WWW || "./";
 
-
-//Mongo connection 
-const MongoClient = require('mongodb').MongoClient;
-var mongoose = require('mongoose');
-var multer = require('multer');
+//Mongo connection
+const MongoClient = require("mongodb").MongoClient;
+var mongoose = require("mongoose");
+var multer = require("multer");
 var Schema = mongoose.Schema;
-const assert = require('assert');
+const assert = require("assert");
 const uri = "mongodb+srv://deep:dnp@4283@cluster0-kdmbn.mongodb.net";
-const dbName = 'mayorwilson';
+const dbName = "mayorwilson";
 
 app.use(express.static(www));
 console.log(`serving ${www}`);
 
-
 app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('./public/pages/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./public/pages/index.html"));
 });
 
-app.use(/*route*/ '/', /*router*/ clientRouter);
+app.use(/*route*/ "/", /*router*/ clientRouter);
 //mongo code
-//schema 
+//schema
 // var ItemSchema = new Schema(
-//   { img: 
+//   { img:
 //       { data: Buffer, contentType: String }
 //   }
 // );
@@ -50,7 +50,6 @@ app.use(/*route*/ '/', /*router*/ clientRouter);
 //   },
 //  }).any());
 
-
 //  app.post('/api/photos',function(req,res){
 //   var newItem = new Item();
 //   newItem.img.data = fs.readFileSync(req.files.userPhoto.path)
@@ -58,17 +57,11 @@ app.use(/*route*/ '/', /*router*/ clientRouter);
 //   newItem.save();
 //  });
 
-
-
-
-
-
-
 /*
-* Sign Up 
-*
-*/
-app.post('/sign_up', function (req, res) {
+ * Sign Up
+ *
+ */
+app.post("/sign_up", function(req, res) {
   console.log(req.body);
   var name = req.body.name;
   var email = req.body.email;
@@ -78,47 +71,50 @@ app.post('/sign_up', function (req, res) {
   var associate_culture_checkbox = req.body.associate_culture_checkbox;
   var tags = req.body.tags;
   var agencies = req.body.agencies;
-
+  var artistWallet = req.body.artistWallet;
+  axios.post(`http://localhost:8080/artistRegister/`, {
+    address: artistWallet
+  });
   var data = {
-    "name": name,
-    "email": email,
-    "password": pass,
-    "biography": biography,
-    "associate": associate,
-    "associate_culture_checkbox": associate_culture_checkbox,
-    "tags": tags,
-    "agencies": agencies
-  }
-  MongoClient.connect(uri, function (err, client) {
+    name: name,
+    email: email,
+    password: pass,
+    biography: biography,
+    associate: associate,
+    associate_culture_checkbox: associate_culture_checkbox,
+    tags: tags,
+    agencies: agencies,
+    artistWallet: artistWallet
+  };
+  MongoClient.connect(uri, function(err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to MongoDB");
     const db = client.db(dbName);
-    db.collection('details').insertOne(data, function (err, collection) {
+    db.collection("details").insertOne(data, function(err, collection) {
       if (err) throw err;
       console.log("Record inserted Successfully");
     });
     client.close();
-  })
+  });
 });
 
-
 /*
-* Login 
-*
-*/
-app.post('/login', function (req, res) {
+ * Login
+ *
+ */
+app.post("/login", function(req, res) {
   console.log(req.body);
   var email = req.body.email;
   var pass = req.body.password;
   var data = {
-    "email": email,
-    "password": pass
-  }
-  MongoClient.connect(uri, function (err, client) {
+    email: email,
+    password: pass
+  };
+  MongoClient.connect(uri, function(err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to MongoDB");
     const db = client.db(dbName);
-    db.collection('details').insertOne(data, function (err, collection) {
+    db.collection("details").insertOne(data, function(err, collection) {
       if (err) throw err;
       console.log("Record inserted Successfully");
     });
@@ -126,7 +122,7 @@ app.post('/login', function (req, res) {
   });
   return null;
 
-  // return res.redirect('signup_success.html'); 
+  // return res.redirect('signup_success.html');
 });
 
-app.listen(port, () => console.log(`listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
