52ae75a2e98de481fe9dc61d67508caeb66c6b76
dnp99 <pateldeep1001@gmail.com>
2020-02-20 22:20:21 -0500

    session login

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const session = require("express-session");
 var bodyParser = require("body-parser");
 const clientRouter = require("./server/routes/route");
 var Grid = require("gridfs-stream");
 var fs = require("fs");
 const app = express();
 const path = require("path");
+const swal = require('sweetalert');
+var cookieParser = require('cookie-parser');
+var flash = require('connect-flash');
+app.use(cookieParser('secret'));
+app.use(session({ cookie: { maxAge: 60000 } }));
+app.use(flash());
 
+app.use(function (req, res, next) {
+  res.locals.success_messages = req.flash('success_messages');
+  res.locals.error_messages = req.flash('error_messages');
+  next();
+});
 //Mongo connection
 const MongoClient = require("mongodb").MongoClient;
 const mongodb = require("mongodb");
@@ -24,13 +36,55 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: false
   })
 );
 
+app.use(session({
+  name: 'sid',
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 2,
+    sameSite: true
+
+  }
+}));
+
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/')
+  } else {
+    next()
+  }
+
+}
+
+// Get - LoginPage
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./public/pages/login.html"));
 });
+
+
+//Get - Index.html
+// app.get("/home", (req, res) => {
+//   console.log(req.session.userId);
+//   if (req.session.userId == undefined) {
+//     res.sendFile(path.resolve("./public/pages/login.html"));
+//   }
+//   else {
+//     res.sendFile(path.resolve("./public/pages/index.html"));
+//   }
+// });
+
+
+app.use('/home', (req, res) => {
+  fs.createReadStream('./public/pages/login.html')
+  .pipe(res);
+});
+
+
 
 app.use(/*route*/ "/", /*router*/ clientRouter);
 
@@ -46,8 +100,9 @@ const handleError = (err, res) => {
     .contentType("text/plain")
     .end("Oops! Something went wrong!");
 };
+
 const upload = multer({
-  dest: "/uploaded/files"
+  dest: "app/uploaded/files"
   // you might also want to set some limits: https://github.com/expressjs/multer#limits
 });
 
@@ -60,18 +115,15 @@ app.post(
     const tempPath = req.file.path;
     console.log(tempPath);
     const targetPath = path.join(__dirname, "./uploads/" + req.body.userPhoto);
-
     // if (path.extname(req.file.originalname).toLowerCase() === ".png") {
-    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+    if (path.extname(req.file.originalname).toLowerCase() === ".jpg") {
       fs.rename(tempPath, targetPath + ".png", err => {
         if (err) return handleError(err, res);
-
         res.redirect("/imageUpload/?" + req.body.userPhoto);
       });
     } else {
       fs.unlink(tempPath, err => {
         if (err) return handleError(err, res);
-
         res
           .status(403)
           .contentType("text/plain")
@@ -85,17 +137,17 @@ app.post(
  * Save photo to mongodb
  *
  */
-app.post("/api/photos", function(req, res) {
+app.post("/api/photos", function (req, res) {
   Grid.mongo = mongoose.mongo;
-  MongoClient.connect(uri, function(err, client) {
+  MongoClient.connect(uri, function (err, client) {
     const db = client.db(dbName);
     var bucket = new mongodb.GridFSBucket(db);
     fs.createReadStream("./public/resources/" + req.body.userPhoto)
       .pipe(bucket.openUploadStream(req.body.userPhoto))
-      .on("error", function(error) {
+      .on("error", function (error) {
         assert.ifError(error);
       })
-      .on("finish", function() {
+      .on("finish", function () {
         console.log("done!");
         client.close();
         res.status(200).send("image saved!");
@@ -107,7 +159,8 @@ app.post("/api/photos", function(req, res) {
  * Sign Up
  *
  */
-app.post("/sign_up", function(req, res) {
+app.post("/sign_up", function (req, res) {
+  swal("Hello world!");
   console.log(req.body);
   var name = req.body.name;
   var email = req.body.email;
@@ -128,62 +181,88 @@ app.post("/sign_up", function(req, res) {
     tags: tags,
     agencies: agencies
   };
-
-  MongoClient.connect(uri, function(err, client) {
+  MongoClient.connect(uri, function (err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to MongoDB");
     const db = client.db(dbName);
-    db.collection("details").insertOne(data, function(err, collection) {
+    db.collection("details").insertOne(data, function (err, collection) {
       if (err) {
         res.send(400).send(err);
         console.log(err);
       }
       console.log("Record inserted Successfully");
-      res.status(200).send("data saved!");
+      res.status(200).send('<script>swal("data saved!")</script>');
     });
     client.close();
   });
 });
 
+
+/**
+ * 
+ * Logout
+ * 
+ */
+app.post('/logout', redirectLogin, (req, res) => {
+
+
+});
 /*
  * Login
  *
  */
-app.post("/login", function(req, res) {
-  console.log(req.body);
-  var email = req.body.email;
-  var pass = req.body.password;
-  var data = {
-    email: email,
-    password: pass
-  };
-  var dbResult;
-  MongoClient.connect(uri, function(err, client) {
-    const db = client.db(dbName);
-    if (err) throw err;
-    db.collection("details")
-      .find({})
-      .toArray(function(err, result) {
-        if (err) throw err;
-        //console.log(result);
-        dbResult = result;
-        client.close();
-        var resV = false;
-        var resData;
-        dbResult.forEach(user => {
-          if (user.email == email && user.password == pass) {
-            resV = true;
-            resData = user;
+app.post("/login", async (req, res) => {
+
+  // if(req.session.userId=='wrong'){
+  //   res.status(400).send('Already Logged In');
+  // }else
+  {
+    var email = req.body.email;
+    var pass = req.body.password;
+    console.log(email);
+    console.log(pass);
+    var data = {
+      email: email,
+      password: pass
+    };
+    var dbResult;
+    MongoClient.connect(uri, function (err, client) {
+      const db = client.db(dbName);
+      if (err) throw err;
+      db.collection("details")
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err;
+          //console.log(result);
+          dbResult = result;
+          client.close();
+          var resV = false;
+          var resData;
+          dbResult.forEach(user => {
+            if (user.email == email && user.password == pass) {
+              resV = true;
+              resData = user;
+            }
+          });
+          console.log("result:" + resV);
+          if (resV) {
+            req.session.userId = resData._id;
+            req.session.userName = resData.name;
+           // console.log(req.session.userId);
+            //res.sendFile(path.resolve("./public/pages/index.html"));
+            res.status(200).json('success!');
+            //res.send("success");
+          }
+          else {
+            // req.flash('error','roor');
+            //res.redirect('/');
+            // res.send("fail");
+            res.status(400).json('oh noes!');
           }
         });
-        console.log("result:" + resV);
-        if (resV) res.sendFile(path.resolve("./public/pages/index.html"));
-        else res.status(404).send("Invalid username and password");
-      });
-  });
-
-  return null;
-
+    });
+    return null;
+  }
   // return res.redirect('signup_success.html');
 });
 
