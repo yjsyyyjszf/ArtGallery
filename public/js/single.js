3da781a0970e6593c70935a3c81466a5715c4b9a
ohyung1287 <ohyung1287@gmail.com>
2020-03-10 12:48:10 -0400

    redirect to metamask

--- public/js/single.js
+++ public/js/single.js
@@ -10,7 +10,7 @@ var artStr = "";
 for (var i = 0; i < artistName.length; i++) artStr += artistName[i] + " ";
 div.innerHTML = `Artist - ${artStr}`;
 const abi = require("./../../abi");
-
+ 
 // var infuraLink =
 //   "https://rinkeby.infura.io/v3/7a19de34892e4625b8464eac960146b7";
 // web3 = new Web3(new Web3.providers.HttpProvider(infuraLink));
@@ -41,25 +41,32 @@ axios
       newBtn.innerHTML = "Buy";
       newBtn.setAttribute("value", `${res.data[i].id}`);
       newBtn.onclick = e => {
-        window.web3 = new Web3(window.ethereum);
-        const DRM = new window.web3.eth.Contract(
-          abi,
-          "0xdea1bdca2d90296face99266ca431920bed90d39"
-        );
-        let id = e.target.value;
-        window.ethereum
-          .enable()
-          .then(async res => {
-            var price = await DRM.methods.tokenPrices(id).call();
-            console.log(price);
-            DRM.methods.requestBuy(id).send({
-              from: res[0],
-              gas: "480000",
-              value: price,
-              gasPrice: web3.utils.toWei("20", "gwei")
-            });
-          })
-          .catch(err => null);
+        console.log('click'+window.ethereum);
+        if (window.ethereum) {
+          window.web3 = new Web3(window.ethereum);
+          const DRM = new window.web3.eth.Contract(
+            abi,
+            "0xdea1bdca2d90296face99266ca431920bed90d39"
+          );
+          let id = e.target.value;
+          window.ethereum
+            .enable()
+            .then(async res => {
+              var price = await DRM.methods.tokenPrices(id).call();
+              console.log(price);
+              DRM.methods.requestBuy(id).send({
+                from: res[0],
+                gas: "480000",
+                value: price,
+                gasPrice: web3.utils.toWei("20", "gwei")
+              });
+            })
+            .catch(err => null);
+        } else {
+          console.log("no metamask");
+          window.location.replace("/metamask");
+        }
+
       };
       newDiv.appendChild(newBtn);
       //   str += `<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item" data-aos="fade" data-src="${res.data[i].thumbnail}"
